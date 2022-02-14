a9c32dd72bef03aa73b999ccb74f43adcb5577fa
BuildTools <cephas8080@outlook.com>
2022-02-14 19:36:45 -0100

    first commit baby

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,26 @@
 <script>
-	export let name;
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+<h1>cephas is a monster lol</h1>
+<div class="monster">monster</div>
 </main>
 
 <style>
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
 		margin: 0 auto;
 	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+    .monster{
+    background: linear-gradient(
+        to right,
+        #365924 0%,
+        #508335 50%,
+        #59ff00 100%
+    );
+    background-clip: text;
+    font-size:20vw;
+    -webkit-text-fill-color: transparent;
+    -webkit-background-clip: text;
+    }
 </style>
